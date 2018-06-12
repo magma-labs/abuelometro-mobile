f59dbefac24239ba15fa8df302f6c0ef3c80231c
luisarmandodv <luisarmandodv@github.com>
2018-06-12 16:17:45 -0500

    [AM-7] => Add IntroduceData Screen design with 'react-native-numeric-input' and a diabetes disease image

--- src/screens/IntroduceData.js
+++ src/screens/IntroduceData.js
@@ -1,12 +1,58 @@
 import React, { Component } from "react";
-import { ScrollView, Text } from "react-native";
+import { Image, StyleSheet, ScrollView, View } from "react-native";
+import NumericInput, { calcSize } from "react-native-numeric-input";
+import { Button, Text } from "react-native-elements";
 
 export default class IntroduceData extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 80
+    };
+  }
+
   render() {
     return (
-      <ScrollView>
-        <Text>Hello world</Text>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Image
+          source={require("../assets/icons/hospital.png")}
+          style={styles.diabetesIcon}
+        />
+        <NumericInput
+          value={this.state.value}
+          onChange={value => this.setState({ value: value })}
+          totalWidth={calcSize(400)}
+          totalHeight={calcSize(150)}
+          rounded
+          textColor="#103900"
+          iconStyle={{ color: "white" }}
+          rightButtonBackgroundColor="#1E5BA9"
+          leftButtonBackgroundColor="#1E5BA9"
+        />
+
+        <Text h4>mg/dl</Text>
+
+        <Button
+          raised
+          title="Guardar"
+          backgroundColor="#6C819B"
+          buttonStyle={{ marginTop: 50 }}
+        />
       </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-start",
+    alignItems: "center"
+  },
+  diabetesIcon: {
+    width: 100,
+    height: 100,
+    marginBottom: 50,
+    marginTop: 25
+  }
+});
